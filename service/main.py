dd211e9d5bca52f29fa2d6aa91e01039c8d6de09
sanjayamal <rajithasanjayamal0918@gmail.com>
2023-06-13 14:32:36 +0530

    inital commit

---
+++ service/main.py
@@ -0,0 +1,7 @@
+from src.app import app
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    app.run()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
