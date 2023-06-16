0543c4ff81ea5ffeb5a29c0706b9b4eb2588dc13
sanjayamal <rajithasanjayamal0918@gmail.com>
2023-06-16 06:24:54 +0530

    update python version 3.9 -> 3.11

--- service/src/app.py
+++ service/src/app.py
@@ -3,7 +3,23 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/api/process-source',methods=['POST'])
+@app.route('/api/v1/<string:user_id>/bot', methods=['POST'])
+def create_bot():
+    return 'Hello, World!'
+
+
+@app.route('/api/v1/<string:user_id>/bots', methods=['get'])
+def get_bots(user_id):
+    return 'Hello, World!'
+
+
+@app.route('/api/v1/<string:user_id>/bot/<string:bot_id>', methods=['get'])
+def get_bot(user_id, bot_id):
+    return 'Hello, World I am new bot!'
+
+
+@app.route('/api/v1/process-source', methods=['POST'])
 def process_source():
     return 'Hello, World!'
+
 
