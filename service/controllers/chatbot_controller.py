880db9d5355bea0132f5c8eeba54e2c0abc2743d
Achintha Weerasinghe <achinthamadumal@gmail.com>
2023-07-26 18:08:45 +0530

    vs-code debugging lunch.json added

--- service/controllers/chatbot_controller.py
+++ service/controllers/chatbot_controller.py
@@ -34,7 +34,7 @@ def create_bot():
 @chatbot_bp.route('/api/v1/bots', methods=['get'])
 @cross_origin(supports_credentials=True)
 # @jwt_verify_middleware
-def get_bots(current_user):
+def get_bots():
     user_id = '550aa922-e98c-477c-9766-0cbea52de9de'
     response = chatbot_service.get_chatbots(user_id)
     return response
